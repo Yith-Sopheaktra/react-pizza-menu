c0accab943fe1da4ea0d6c6f85255eb78da51139
Yith Sopheaktra <yithsopheaktra18@gmail.com>
2023-11-06 23:56:24 +0700

    Use props to pass data from parent component to child

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
 	{
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
 	return (
-		<div>
+		<div className="container">
 			<Header />
 			<Menu />
 			<Footer />
@@ -57,40 +58,61 @@ function App() {
 }
 
 function Header() {
+	const style = {
+		color: "red",
+		fontSize: "48px",
+		textTransform: "uppercase",
+	};
+
 	return (
 		<div>
-			<h1 style={{ color: "red" }}>Fast React Pizza Co.</h1>
+			<header className="header">
+				<h1>Fast React Pizza Co.</h1>
+			</header>
 		</div>
 	);
 }
 
 function Menu() {
 	return (
-		<div>
-			<h2>Our Menu</h2>
-			<Pizza />
-			<Pizza />
-			<Pizza />
-			<Pizza />
+		<main>
+			<div className="menu">
+				<h2>Our Menu</h2>
+				<Pizza
+					name="Pizza Prosciutto"
+					ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+					photoName="pizzas/spinaci.jpg"
+					price={10}
+				/>
+				<Pizza
+					name="Pizza Funghi"
+					ingredients="Tomato, mozarella, mushrooms, and onion"
+					photoName="pizzas/funghi.jpg"
+					price={13}
+				/>
+			</div>
+		</main>
+	);
+}
+
+function Pizza(props) {
+	return (
+		<div className="pizza">
+			<img src={props.photoName} alt={props.name} />
+			<div>
+				<h3>{props.name}</h3>
+				<p>{props.ingredients}</p>
+				<span>{props.price +5}</span>
+			</div>
 		</div>
 	);
 }
 
 function Footer() {
 	return (
-		<footer>
+		<footer className="footer">
 			{new Date().toLocaleTimeString()} We are currently open.
 		</footer>
-	);
-}
-
-function Pizza() {
-	return (
-		<div>
-			<img src="pizzas/spinaci.jpg" alt="#" />
-			<h1>Pizza Prosciutto</h1>
-			<p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
-		</div>
 	);
 }
 
